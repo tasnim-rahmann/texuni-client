76d75a8f8f3f178fc926d4e6a551400f0f09d00d
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-11-18 16:07:33 +0600

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-lg font-medium uppercase">
+        Ph University management app
+      </h1>
+    </>
+  );
+}
+
+export default App;
